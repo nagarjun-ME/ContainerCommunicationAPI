499fbb486f6a6b46aa7a12fdd112d9d3058990e5
nagarjun-ME <nagarjun.mqdev08@gmail.com>
2020-10-31 15:13:42 -0400

    new post endpoint added

--- src/main/java/com/naga/docker/mpc/ContainerCommunicationAPI/RPCRestService.java
+++ src/main/java/com/naga/docker/mpc/ContainerCommunicationAPI/RPCRestService.java
@@ -1,8 +1,11 @@
 package com.naga.docker.mpc.ContainerCommunicationAPI;
 
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -22,5 +25,12 @@ public class RPCRestService {
 	public String getMessage() {
 		log.info("inside get message method");
 		return "Hi!! How are You?";
+	}
+	
+	@PostMapping("/dt")
+	public String setDate(String date) {
+		log.info("inside get date method");
+		String str= "<h1>Date is :"+new Date().toString()+"</h1>";
+		return str;
 	}
 }
