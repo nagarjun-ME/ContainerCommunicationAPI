f998d5c341d5ec1cba76a1ce4e60d9b43e760476
nagarjun-ME <nagarjun.mqdev08@gmail.com>
2020-10-31 14:04:38 -0400

    Added a rest method

--- src/main/java/com/naga/docker/mpc/ContainerCommunicationAPI/RPCRestService.java
+++ src/main/java/com/naga/docker/mpc/ContainerCommunicationAPI/RPCRestService.java
@@ -2,11 +2,19 @@ package com.naga.docker.mpc.ContainerCommunicationAPI;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
-
+@RequestMapping("/to")
 public class RPCRestService {
+	
 	
 	private Logger log=LoggerFactory.getLogger(RPCRestService.class);
 
+	@GetMapping
+	public String getResponse() {
+		log.info("inside get response method");
+		return "success";
+	}
 }
