c858e560cc8cf510c33da596771532805dbd534e
nagarjun-ME <nagarjun.mqdev08@gmail.com>
2020-10-31 15:51:00 -0400

    Handling input paramates from client

--- src/main/java/com/naga/docker/mpc/ContainerCommunicationAPI/RPCRestService.java
+++ src/main/java/com/naga/docker/mpc/ContainerCommunicationAPI/RPCRestService.java
@@ -6,27 +6,31 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-
-@RequestMapping("/to")
+@RestController
+@RequestMapping("/")
 public class RPCRestService {
 	
 	
 	private Logger log=LoggerFactory.getLogger(RPCRestService.class);
 
-	@GetMapping
+	@GetMapping("/")
 	public String getResponse() {
 		log.info("inside get response method");
 		return "success";
 	}
 	
 	@GetMapping("/msg")
-	public String getMessage() {
-		log.info("inside get message method");
-		return "Hi!! How are You?";
+	public String getMessage(@RequestParam String name ) {
+		log.info("inside get message method -"+name);
+		return "Hi!! How are You?"+name;
 	}
 	
+
 	@PostMapping("/dt")
 	public String setDate(String date) {
 		log.info("inside get date method");
